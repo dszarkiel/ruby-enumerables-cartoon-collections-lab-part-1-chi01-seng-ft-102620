9f2d167ff01588ae4c93e6ca7f4598d090209593
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-09 16:13:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index(1) do |name, index|
+  array.each.with_index(1) do |name, index|
     puts "#{index} #{name}"
 end
 
